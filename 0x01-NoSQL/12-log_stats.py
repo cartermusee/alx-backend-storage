250bb5e02022806ac7ead7af7678ba0f2479b35f
cartermusee <cartermusee@gmail.com>
2024-01-23 18:33:57 +0300

    using python to update

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+
+    cluster = MongoClient('mongodb://localhost:27017')
+    db = cluster['logs']
+    collection = db['nginx']
+
+    count_docs = collection.count_documents({})
+    print("{} logs".format(count_docs))
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(status_count))
